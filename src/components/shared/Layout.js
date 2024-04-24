9a10698d4b7861b93c94f41d05b196e0b7f7ece4
Bhanu Pratap Singh <bp.mnnit@gmail.com>
2024-04-24 18:39:09 +0530

    Added the Header Component and its being coded

--- src/components/shared/Layout.js
+++ src/components/shared/Layout.js
@@ -1,12 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import Sidebar from './Sidebar';
+import Header from './Header';
 
 const Layout = () => {
   return (
     <div className='flex flex-row bg-neutral-100 h-screen w-screen overflow-hidden'>
       <Sidebar />
-      <div className='p-4'>
-        <div className='bg-teal-200'>Header</div>
+      <div className='flex-1'>
+        <Header />
         <div>{<Outlet />}</div>
       </div>
     </div>
